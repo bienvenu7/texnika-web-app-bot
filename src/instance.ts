79749d5ab460c8035f46f734d0f62fbaef7b89ac
bienvenu7 <ngadzieb@gmail.com>
2024-05-18 16:45:29 +0300

    first commith

--- src/instance.ts
+++ src/instance.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://afue-api.onrender.com/"
+      ? "https://emfcc-backend.onrender.com/"
       : "http://localhost:8181/",
 });
