0de0b9652cae1d27b82e815eaac24ae23fc58851
bienvenu7 <ngadzieb@gmail.com>
2024-06-03 01:55:10 +0300

    added now

--- src/components/home/Cover.tsx
+++ src/components/home/Cover.tsx
@@ -1,12 +1,15 @@
+import { selectUserId } from "@/redux/selectors/productSelector";
 import React from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
 const Cover = (props: Props) => {
+  const userId = useSelector(selectUserId);
   return (
     <div className="cover__container">
       <div className="cover__box">
-        <h1>50% Off</h1>
+        <h1>50% Off {userId}</h1>
         <p>On minimun 5 quantity bougth today</p>
         <button>Get Now</button>
       </div>
